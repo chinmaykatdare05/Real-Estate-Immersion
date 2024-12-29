4ad94e570d2fb489e6f234baa17a145edbc47c82
chinmaykatdare05 <chinmayy05@gmail.com>
2024-12-29 20:15:00 +0530

    added fonts, changed explore page, property detail page and profile page. corrected the logic for logout

--- lib/pages/forgot_password/forgot_password.dart
+++ lib/pages/forgot_password/forgot_password.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:flutter03/services/auth_service.dart';
 import 'package:fluttertoast/fluttertoast.dart';
