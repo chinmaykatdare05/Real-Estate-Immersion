1c3463b5d1b1ff97db06d0d1776352dc4146d80c
aditya00025 <143825521+aditya00025@users.noreply.github.com>
2025-01-11 17:47:00 +0530

    Update profile.dart

--- lib/pages/home/profile.dart
+++ lib/pages/home/profile.dart
@@ -3,7 +3,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter03/pages/home/home.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -61,28 +60,32 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
         return Scaffold(
           body: SingleChildScrollView(
-            padding: const EdgeInsets.all(20.0),
+            padding: const EdgeInsets.fromLTRB(20.0, 60.0, 20.0, 20.0), // Added padding at the top
             child: Column(
               children: [
                 ListTile(
                   leading: CircleAvatar(
-                    radius: 36,
-                    backgroundColor: Colors.blue[700],
+                    radius: 30,
+                    backgroundColor: const Color.fromARGB(255, 0, 0, 0),
                     child: Text(
                       userData['name'][0].toUpperCase(),
-                      style: const TextStyle(fontSize: 30, color: Colors.white),
+                      style: const TextStyle(
+                          fontSize: 30, color: Colors.white),
                     ),
                   ),
                   title: Text(
                     userData['name'],
                     style: const TextStyle(fontSize: 24),
                   ),
-                  subtitle: const Text('RI user'),
+                  subtitle: const Text('Real Estate Intelligence '),
                   onTap: () {},
                   trailing: const SizedBox(height: 30),
                 ),
                 const SizedBox(height: 15),
-                const Divider(),
+                const Divider(
+                    color: Colors.black,  // Change this color to whatever you want
+                    thickness: 3.0,  // Adjust this value to make the divider bold (thicker)
+                ),
                 const ListTile(
                   title: Text(
                     'Personal Information',
@@ -107,6 +110,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   title: const Text('PAN Card Number'),
                   subtitle: Text(userData['panCard']),
                 ),
+                const Divider(
+                    color: Colors.black,  // Change this color to whatever you want
+                    thickness: 3.0,  // Adjust this value to make the divider bold (thicker)
+                ),
                 const ListTile(
                   title: Text(
                     'Settings',
@@ -122,9 +129,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   onTap: () => _navigateToPersonalInfo(userData),
                 ),
                 ListTile(
-                    leading: const Icon(Icons.password),
-                    title: const Text('Change Password'),
-                    onTap: () {}),
+                  leading: const Icon(Icons.password),
+                  title: const Text('Change Password'),
+                  onTap: () {},
+                ),
                 ListTile(
                   leading: const Icon(Icons.logout),
                   title: const Text(
@@ -161,107 +169,116 @@ class PersonalInfoScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final TextEditingController usernameController =
         TextEditingController(text: name);
+    final TextEditingController emailController =
+        TextEditingController(text: email);
     final TextEditingController phoneController =
         TextEditingController(text: phone);
     final TextEditingController panController =
         TextEditingController(text: pan);
 
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-              const Text(
-                'Update your personal information',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 30),
-              TextField(
-                controller: usernameController,
-                decoration: InputDecoration(
-                  labelText: 'Name',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Update your personal information',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                   ),
-                  prefixIcon: const Icon(Icons.person),
                 ),
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: phoneController,
-                decoration: InputDecoration(
-                  labelText: 'Phone Number',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
+                const SizedBox(height: 30),
+                TextField(
+                  controller: usernameController,
+                  decoration: InputDecoration(
+                    labelText: 'Name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    prefixIcon: const Icon(Icons.person),
                   ),
-                  prefixIcon: const Icon(Icons.phone),
                 ),
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: panController,
-                decoration: InputDecoration(
-                  labelText: 'PAN Card Number',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    prefixIcon: const Icon(Icons.email),
                   ),
-                  prefixIcon: const Icon(Icons.credit_card),
                 ),
-              ),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () {
-                  FirebaseFirestore.instance
-                      .collection('users')
-                      .doc(FirebaseAuth.instance.currentUser!.uid)
-                      .update({
-                    'name': usernameController.text,
-                    'phoneNumber': phoneController.text,
-                    'panCard': panController.text,
-                  }).then((_) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                          content: Text('Profile updated successfully!')),
-                    );
-                    Navigator.pushAndRemoveUntil(
+                const SizedBox(height: 20),
+                TextField(
+                  controller: phoneController,
+                  decoration: InputDecoration(
+                    labelText: 'Phone Number',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    prefixIcon: const Icon(Icons.phone),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: panController,
+                  decoration: InputDecoration(
+                    labelText: 'PAN Card Number',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    prefixIcon: const Icon(Icons.credit_card),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () {
+                    FirebaseFirestore.instance
+                        .collection('users')
+                        .doc(FirebaseAuth.instance.currentUser!.uid)
+                        .update({
+                      'name': usernameController.text,
+                      'email': emailController.text,
+                      'phoneNumber': phoneController.text,
+                      'panCard': panController.text,
+                    }).then((_) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content: Text('Profile updated successfully!')),
+                      );
+                    }).catchError((error) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                            content: Text('Failed to update profile: $error')),
+                      );
+                    });
+                    Navigator.pushReplacement(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => const HomePage(),
+                        builder: (context) => const ProfileScreen(),
                       ),
-                      (route) => false,
                     );
-                  }).catchError((error) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                          content: Text('Failed to update profile: $error')),
-                    );
-                  });
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(vertical: 15),
-                  backgroundColor: Colors.blue[700],
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    backgroundColor: Colors.blue[700],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    minimumSize: const Size(double.infinity, 60),
                   ),
-                  minimumSize: const Size(double.infinity, 60),
+                  child: const Text(
+                    'Update Profile',
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
                 ),
-                child: const Text(
-                  'Update Profile',
-                  style: TextStyle(fontSize: 16, color: Colors.white),
-                ),
-              )
-            ],
+              ],
+            ),
           ),
         ),
       ),
